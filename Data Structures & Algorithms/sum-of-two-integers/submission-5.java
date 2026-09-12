ea9b3496240a21eceb59b84d59e09f6f6952280f
Ashwini Abhishek <60640904+ashwiniabhishek@users.noreply.github.com>
2026-09-12 16:54:37 +0530

    Add: sum-of-two-integers - submission-5

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int c = 0;
+        int ans = 0;
+        int count = 0;
+        boolean twoComplementAddition = (a < 0 || b < 0);
+        for(int i = 0; i<32; i++) {
+            ans |= (((a & 1) ^ (b & 1) ^ c)<<i);
+            c = (a & 1 & b & 1) | (b & 1 & c & 1) | (a & 1 & c & 1);
+            a>>>=1;
+            b>>>=1;
+        }
+        return ans;
+    }
+}
