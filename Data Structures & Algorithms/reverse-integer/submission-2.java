0cf84b3f270338515a923162d7258f1640b4267d
Ashwini Abhishek <60640904+ashwiniabhishek@users.noreply.github.com>
2026-09-15 01:07:02 +0530

    Add: reverse-integer - submission-2

---
+++ Data Structures & Algorithms/reverse-integer/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        int digit = 0;
+        int threshold = Integer.MAX_VALUE/10;
+        int threshold2 = Integer.MIN_VALUE/10;
+        while (x!=0) {
+            digit = (x % 10);
+            x = x/10;
+            ans = ans * 10 + digit;
+            if (ans > threshold && x != 0) {
+                return 0;
+            }
+            if (ans < threshold2 && x != 0) {
+                return 0;
+            };
+            if (ans == threshold) {
+                digit = (x % 10);
+                if (digit > 7) return 0;
+            } 
+            if (ans == threshold2) {
+                digit = (x % 10);
+                if (digit < -8) return 0;
+            }
+        }
+        return ans;
+    }
+}
